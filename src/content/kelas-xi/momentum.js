4ce57234c7c3316883bc1dcedfbf328f7b36a65e
Riana Alazri <riana.alfazri@gmail.com>
2026-06-01 09:07:38 +0700

    penambahan konten momentum

--- src/content/kelas-xi/momentum.js
+++ src/content/kelas-xi/momentum.js
@@ -5,11 +5,15 @@ const momentum = {
   content: [
     {
       heading: "Pengertian Momentum",
-      body: "Momentum adalah hasil kali massa benda dengan kecepatannya. Semakin besar massa atau kecepatan suatu benda, semakin besar momentumnya."
+      body: "Momentum adalah ukuran  seberapa besar gerakan suatu benda. Semakin besar massa benda semakin cepat gerak benda maka momentumnya makin besar."
     },
     {
       heading: "Rumus Momentum",
-      body: "p = m × v, di mana p adalah momentum (kg·m/s), m adalah massa (kg), dan v adalah kecepatan (m/s)."
+      body: "p = m × v, di mana p adalah momentum (kg·m/s), m adalah massa (kg), dan v adalah kecepatan (m/s). Jika massa diperbesar atau kecepatan ditingkatkan, momentum akan meningkat. Sebaliknya, jika massa atau kecepatan dikurangi, momentum akan menurun."
+    },
+    {
+      heading: "Arah Momentum",
+      body: "Momentum memiliki arah yang sama dengan kecepatan benda. Jika benda bergerak ke kanan, momentum juga ke kanan. Jika benda bergerak ke kiri, momentum juga ke kiri."
     },
     {
       heading: "Hukum Kekekalan Momentum",
