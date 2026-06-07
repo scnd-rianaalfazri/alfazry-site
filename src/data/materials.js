0aea0aea0f9a5880f10ddff07c34ad8fb5667153
Riana Alazri <riana.alfazri@gmail.com>
2026-06-08 06:32:18 +0700

    fix: correct case-sensitive import path for gelombang.js and asset filename

--- src/data/materials.js
+++ src/data/materials.js
@@ -1,5 +1,5 @@
 import Fluida from "../content/materi/kelas-xi/Fluida.js"
-import Gelombang from "../content/materi/kelas-xi/Gelombang.js"
+import Gelombang from "../content/materi/kelas-xi/gelombang.js"
 import PengertianMomentum from "../content/materi/kelas-xi/PengertianMomentum.js"
 import pengertianimpuls from "../content/materi/kelas-xi/PengertianImpuls.js"
 import HukumKekekalanMomentum from "../content/materi/kelas-xi/HukumKekekalanMomentum.js"
